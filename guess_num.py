3c6b56f204d7f42eaf8f38bdf291b84eea431e6e
Gundamwindcar <xubiying88@gmail.com>
2019-08-21 08:48:18 -0700

    add range

--- guess_num.py
+++ guess_num.py
@@ -1,6 +1,10 @@
 import random
 
-r = random.randint(1,100)
+start = input('Please enter the start of the random numbers:')
+end = input('Please enter the end of the random numbers:')
+start = int(start)
+end = int(end)
+r = random.randint(start, end)
 count = 0
 
 while True:
